2d4856a791ab8acf1dd57e0f88f7ccd0b19de45d
nunoalves233 <nunoalves233@gmail.com>
2024-04-16 11:27:56 +0100

    improved python file

--- python_basic_training/percentage_male_female_v2.py
+++ python_basic_training/percentage_male_female_v2.py
@@ -1,5 +1,5 @@
 n = int(input())
 names = [input() for i in range(n)]
-female_names = [name for name in names if name[-1] == 'a']
+female_names = [name for name in names if name[-1] == 'a' or name[-1] == 'z']
 female_percent = len(female_names) / n * 100
 print(f"Male: {(100-female_percent):.2f}%, Female: {female_percent:.2f}%")
